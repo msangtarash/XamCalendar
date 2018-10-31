ab9e8d9c7354039d920ea59e82ad58256135a6ab
Marzie Sangtarash <m.sangtarash@outlook.com>
2018-10-31 17:44:46 +0330

    Initial popup page

--- XamCalendar/XamCalendar/ManualTest/TestViewModel.cs
+++ XamCalendar/XamCalendar/ManualTest/TestViewModel.cs
@@ -1,9 +1,26 @@
-﻿using System.ComponentModel;
+﻿using NodaTime;
+using Rg.Plugins.Popup.Services;
+using System.ComponentModel;
+using Xamarin.Forms;
 
 namespace XamCalendar
 {
-    public class TestViewModel : INotifyPropertyChanged
+    public class TestViewModel : INotifyPropertyChanged 
     {
+        private CalenderPopupView _testPopupPage;
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public Command OpenPopupCommand { get; set; }
+
+        public TestViewModel()
+        {
+            OpenPopupCommand = new Command( async () =>
+            {
+                _testPopupPage = new CalenderPopupView();
+
+                await PopupNavigation.Instance.PushAsync(_testPopupPage);
+            });
+        }
     }
 }
